ecf02626fe3d457d571adf00ebc4373bfc125f0a
saurabh <saurabh15dakle@gmail.com>
2023-04-24 12:36:13 +0530

    setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.static("public"))
+
+app.use("/api/users", (req, res) => {
+    res.json({
+        message: "user fetch success"
+    })
+})
+
+app.use("*", (req, res) => {
+    res.sendFile("public/index.html")
+})
+
+app.listen(process.env.PORT || 5000, console.log("running"))
